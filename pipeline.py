8d1b4fbd58ab588888fa8de360cad9e4807eadb8
Nosherwan <nosherwansheikh@gmail.com>
2026-05-22 05:43:23 +0500

    pipeline complete

--- pipeline.py
+++ pipeline.py
@@ -2,64 +2,48 @@ import requests
 from dotenv import load_dotenv
 import os
 from datetime import datetime
-import pandas as pd
 import psycopg2
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 load_dotenv()
-print(os.getenv("DB_URL"))
-conn=psycopg2.connect(os.getenv("DB_URL"))
-cursor=conn.cursor()
-
-
-print("KEY:", os.getenv("API_KEY"))
 
 API_KEY = os.getenv("API_KEY")
+DB_URL = os.getenv("DB_URL")
 CITY = "Karachi"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
-res = requests.get(url)
-logger.info(f"Fetched weather for {CITY}: {data['main']['temp']} degree celsisus")
+try:
+    
+    conn = psycopg2.connect(DB_URL)
+    cursor = conn.cursor()
 
-print(res)
-print()
 
-data = res.json()
-#print(res.text)
-print()
-# print(data)
+    res = requests.get(url)
+    data = res.json()
 
-print(data["main"]["temp"])
-print(data["main"]["humidity"])
-print(data["weather"][0]["description"])
+    logger.info(f"Fetched weather for {CITY}: {data['main']['temp']}°C")
 
-data = res.json()
-
-row = {
+    row = {
     "city": data["name"],
     "temp": data["main"]["temp"],
     "humidity": data["main"]["humidity"],
     "weather": data["weather"][0]["description"],
-}
-print(row)
+    "timestamp": datetime.now(),
+    }
 
-print()
+    cursor.execute(
+    "INSERT INTO weather_log (city, temp, humidity, weather, timestamp) VALUES (%s, %s, %s, %s, %s)",
+    (row["city"], row["temp"], row["humidity"], row["weather"], row["timestamp"]),
+    )
 
-row["timestamp"] = datetime.now()
+    conn.commit()
+    conn.close()
 
-print(row)
+    logger.info("Row inserted into DB successfully")
 
-df = pd.DataFrame([row])
-print(df)
-
-cursor.execute("insert into weather_log (city, temp, humidity, weather, timestamp) values (%s, %s, %s, %s, %s)", (row["city"], row["temp"], row["humidity"], row["weather"], row["timestamp"]))
-
-conn.commit()
-conn.close()
-
-logger.info("Row inserted into db successfully")
+except Exception as e:
+    logger.error(f"something went wrong: {e}")
